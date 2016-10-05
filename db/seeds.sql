ac40daa55a210bb6ad5ba8483bc5448a6dc59320
Brian Tom <briantom171@gmail.com>
2016-10-04 22:29:20 -0700

    maybe?

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', FALSE);
+
+SELECT * FROM burgers;
